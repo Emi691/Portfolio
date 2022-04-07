e2278a1a0a0a8bcdc44087cbbbd9c03fe966417c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-04-07 12:37:10 -0700

    edits imports and exports for contact component

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import './contact.scss'
 
-function contact(props) {
+export default function contact() {
     return (
         <div>
             contact
         </div>
     );
 }
-
-export default contact;
