22fe24d4a7de822476c77cd77086f023fb575573
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-04-20 20:37:50 -0700

    sets up wrapper div containing left and right divs in navigation component

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -3,7 +3,14 @@ import './navigation.scss'
 export default function Navigation(props) {
     return (
         <div className='nav'>
-            navigation
+            <div className='wrapper'>
+                <div className='left'>
+                    left
+                </div>
+                <div className='right'>
+                    right
+                </div>
+            </div>
         </div>
     );
 }
