4cfe7e43ebc48a16e3f5860e328a78de8195deaf
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-04-26 17:35:37 -0700

    renders links to main components in side

--- src/components/navigation/side.jsx
+++ src/components/navigation/side.jsx
@@ -3,7 +3,20 @@ import './side.scss'
 export default function side() {
     return (
         <div className='side' id='side'>
-            side
+            <ul>
+                <li>
+                    <a href="#intro">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#works">Works</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
         </div>
     );
 }
