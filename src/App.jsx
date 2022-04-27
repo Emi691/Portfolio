210ee6536b5b0af75ed198008207c4be26426550
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-04-27 14:07:29 -0700

    sets sidebar state in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
+import {useState} from 'react';
 import Navigation from './components/navigation/navigation';
-import Side from './components/navigation/side'
-import About from './components/about/about'
-import Contact from './components/contact/contact'
-import Intro from './components/intro/intro'
-import Works from './components/works/works'
+import Side from './components/navigation/side';
+import About from './components/about/about';
+import Contact from './components/contact/contact';
+import Intro from './components/intro/intro';
+import Works from './components/works/works';
 
 function App() {
+  const [sidebar, setSidebar] = useState(true)
   return (
     <div className="App">
       <Navigation/>
