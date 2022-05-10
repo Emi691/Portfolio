ed6856a36d09656ec7cbc2dffbcb370b96b8d6d8
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-09 17:46:02 -0700

    creates scaffolding for text and text animation in intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,10 @@ import './intro.scss'
 export default function intro() {
     return (
         <div className='intro' id="intro">
-            intro
+            <div className='text'>
+                <div className='hello'></div>
+                <div className="roles"></div>
+            </div>
         </div>
     );
 }
