da8d969323526dd946c64ff25fe5554088a44084
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-10 18:04:36 -0700

    changes render formatting of intro component

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -2,19 +2,20 @@ import './intro.scss'
 import {useState} from 'react';
 import Typewriter from 'typewriter-effect';
 
-export default function intro() {
+export default function Intro() {
     const [roles, setRoles] = useState(['Software Engineer', 'Scientist', 'Creator'])
     return (
         <div className='intro' id="intro">
+            <h1>Hi, I'm Emiko</h1>
             <div className='wrapper'>
-                <h1>Hi, I'm Emiko</h1>
-                <h2>I'm a <Typewriter
+                <h2>I'm a</h2>
+                <Typewriter
                 options={{
                     strings: roles,
                     autoStart: true,
                     loop: true,
                 }}
-                /></h2>
+                />  
             </div>
         </div>
     );
