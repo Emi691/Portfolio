8e3d87dd8f62bd2014762ba28a7450a71fdae56a
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-10 17:54:01 -0700

    creates typewriter animation in intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,11 +1,18 @@
 import './intro.scss'
+import Typewriter from 'typewriter-effect';
 
 export default function intro() {
     return (
         <div className='intro' id="intro">
             <div className='wrapper'>
                 <h1>Hi, I'm Emiko</h1>
-                <h2>I'm a <span></span></h2>
+                <h2>I'm a <Typewriter
+                options={{
+                    strings: ['Software Engineer', 'Scientist', 'Creator'],
+                    autoStart: true,
+                    loop: true,
+                }}
+                /></h2>
             </div>
         </div>
     );
