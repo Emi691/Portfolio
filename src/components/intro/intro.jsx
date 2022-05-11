6e07a45bcfbe33cd0345f1d41cd7621886c27f0d
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-10 17:56:11 -0700

    stores and renders roles array in state of intro component

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,14 +1,16 @@
 import './intro.scss'
+import {useState} from 'react';
 import Typewriter from 'typewriter-effect';
 
 export default function intro() {
+    const [roles, setRoles] = useState(['Software Engineer', 'Scientist', 'Creator'])
     return (
         <div className='intro' id="intro">
             <div className='wrapper'>
                 <h1>Hi, I'm Emiko</h1>
                 <h2>I'm a <Typewriter
                 options={{
-                    strings: ['Software Engineer', 'Scientist', 'Creator'],
+                    strings: roles,
                     autoStart: true,
                     loop: true,
                 }}
