85f30bdc90c13fa031346d251d01f3d4ed605cf9
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-16 17:28:54 -0700

    imports face icon and sets as alt for image

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,10 @@
 import './about.scss'
+import FaceIcon from '@mui/icons-material/Face';
 
 export default function About() {
     return (
         <div className='about' id="about">
-            <img src="assets/me.jpg" alt=""/>
+            <img src="assets/me.jpg" alt={FaceIcon}/>
         </div>
     );
 }
