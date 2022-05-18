aeffbf25c9831e476cace62f52a2910dca5a3fde
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-17 17:17:01 -0700

    styles 'hi' div in about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,11 +4,11 @@ import FaceIcon from '@mui/icons-material/Face';
 export default function About() {
     return (
         <div className='about' id="about">
-            <div className="image">
+            <div className="me">
                 <img src="assets/me.jpg" alt={FaceIcon}/>
             </div>
-            <div className="me">
-                <p>I am a spunky, inquisitive software engineer. I code in Ruby and JavaScript, frequently within with Rails and React.
+            <div className="hi">
+                <p>Hi! My name is Emiko. I am a spunky, inquisitive software engineer. I code in Ruby and JavaScript, frequently within with Rails and React.
                  I blend strong analytical and problem-solving skills from my background in chemistry with software engineering aptitude to write clean, efficient code.
                  When I am not at my desk, you can find me climbing at the crag, sewing a new item for my wardrobe, or baking up delicous treats. 
                  </p>
