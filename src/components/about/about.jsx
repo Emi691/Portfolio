716720d1a9acb1ea1b6655b14b3deee4ae396895
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-05-17 17:12:55 -0700

    adds paragraph describing me in about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,6 +8,10 @@ export default function About() {
                 <img src="assets/me.jpg" alt={FaceIcon}/>
             </div>
             <div className="me">
+                <p>I am a spunky, inquisitive software engineer. I code in Ruby and JavaScript, frequently within with Rails and React.
+                 I blend strong analytical and problem-solving skills from my background in chemistry with software engineering aptitude to write clean, efficient code.
+                 When I am not at my desk, you can find me climbing at the crag, sewing a new item for my wardrobe, or baking up delicous treats. 
+                 </p>
             </div>
             
         </div>
